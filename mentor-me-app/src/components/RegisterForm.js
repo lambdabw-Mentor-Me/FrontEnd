e544e86728c57f4b300b1d74c9a2b628bfb3a5d0
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-09-23 11:01:27 -0400

    added register form

--- mentor-me-app/src/components/RegisterForm.js
+++ mentor-me-app/src/components/RegisterForm.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const RegisterForm = () => {
+    const [user, setUser] = useState({
+        credentials: {
+            email: '',
+            password: ''
+        }
+    })
+
+    const handleChange = e => {
+        setUser({
+            credentials: {
+                ...user.credentials,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        axiosWithAuth().post('/ent/register', user.credentials)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => console.log(err.response))
+    }
 
     return (
-        <h1>Hello from registerform.js</h1>
+        <form onSubmit={register}>
+          <input
+            type="email"
+            name="username"
+            value={user.credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={user.credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
     )
 } 
 
