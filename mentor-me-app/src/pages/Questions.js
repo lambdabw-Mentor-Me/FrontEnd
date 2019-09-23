5d1ef571426ef554057887c28b0b06eab3773178
GUL_Bao <bao@new-host-3.home>
2019-09-23 18:52:00 -0400

    info div template styles && axios image call

--- mentor-me-app/src/pages/Questions.js
+++ mentor-me-app/src/pages/Questions.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
+import axios from 'axios';
+
+// * STYLE IMPORTS (style-components)
+import style from './StyledQuestions';
 
 const Questions = () => {
 
+    // * useState for temporary profile images
+    const [pic, setPic] = useState({});
+
+    // ? AXIOS CALL FOR USER INFORMATION (User pictures)
+    useEffect(() => {
+        axios.get(`https://randomuser.me/api/?inc=picture`)
+        .then(res => res.data.results[0])
+        .then(res => setPic(res.picture))
+    }, [])
+
+    // ! LOG DATA
+    console.log(pic)
+
     // ? MOMENT PACKAGE PROVIDING LOCAL TIME TO USERS.
+    // TODO: Implement
     console.log(moment().format('LT'))
 
     return (
-        <section className={`questions__Container`}>
+        <style.section>
             {/* // * TOP DIV PROVIDING INFO (TIME, USERNAME, PIC, ETC.)
              */}
-            <div className={`questions__info`}>
-                <h2>USERNAME HERE</h2>
-            </div>
+            <style.infoDiv>
+                <div className={`info__top`}>
+                    <h2>USERNAME HERE</h2>
+                </div>
+
+                <div className={`info__bottom`}>
+                    <img src={`${pic.medium}`} alt="Profile image"/>
+                </div>
+            </style.infoDiv>
 
             {/* // * MIDDLE DIV FOR POSTED QUESTIONS & DESCRIPTION
              */}
-            <div className={`questions__description`}>
+            <style.descriptionDiv>
                 <h3>DESCRIPTION HERE:</h3>
-            </div>
+            </style.descriptionDiv>
 
             {/* // * BUTTON FOR POSTED QUESTIONS & DESCRIPTION
              */}
-            <div className={`questions__btn`}>
+            <style.btnDiv>
                 <button>
                 {/* // TODO: IMG icon here.
                  */}
                 Respond button here
                 </button>
-            </div>
-        </section>
+            </style.btnDiv>
+        </style.section>
     )
 }
 
---
+++ mentor-me-app/src/pages/StyledQuestions.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const section = styled.section`
+    width: 100vw;
+    height: 100vh;
+    background-color: red;
+
+    & > * {
+        border: none;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
+
+const infoDiv = styled.div`
+
+    height: 20vh;
+
+    & > .info__top {
+        height: 80%;
+        color: #FEFEFF;
+        background-image: linear-gradient(90deg, #5574f7 0%, #60c3ff 100%);
+
+        & > h2 {
+            padding-top: 2rem;
+            text-align: center;
+        }
+    }
+
+    & > .info__bottom {
+        background-color: #FFFFFF;
+        height: 30%;
+
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+    }
+`;
+
+const descriptionDiv = styled.div`
+    height: 65vh;
+    background-color: #FAFAFA;
+`;
+
+const btnDiv = styled.div`
+    height: 5vh;
+
+    & > button {
+        background-color: #60C3FF;
+    }
+`;
+
+export default {
+    section,
+    infoDiv,
+    descriptionDiv,
+    btnDiv
+}
