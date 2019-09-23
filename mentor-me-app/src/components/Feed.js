843a0b6685c1df7eeeb2b0315fa04292757c1a7e
Michael Harms <michael.harms6010@gmail.com>
2019-09-23 13:15:46 -0500

    added headers to default text color rule, labels

---
+++ mentor-me-app/src/components/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-comonents";
+
+
+
+const Feed = (props) => {
+
+    return (
+        <h1> I'm a feed component!</h1>
+    )
+} 
+
+export default LoginForm;
