a4093853e4b112681d31a4b084f9221aabfab25c
GUL_Bao <bao@new-host-3.home>
2019-09-23 14:48:49 -0400

    questions template created

--- mentor-me-app/src/pages/Questions.js
+++ mentor-me-app/src/pages/Questions.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import moment from 'moment';
 
 const Questions = () => {
 
+    // ? MOMENT PACKAGE PROVIDING LOCAL TIME TO USERS.
+    console.log(moment().format('LT'))
+
     return (
-        <h1>Hello from questions.js</h1>
+        <section className={`questions__Container`}>
+            {/* // * TOP DIV PROVIDING INFO (TIME, USERNAME, PIC, ETC.)
+             */}
+            <div className={`questions__info`}>
+                <h2>USERNAME HERE</h2>
+            </div>
+
+            {/* // * MIDDLE DIV FOR POSTED QUESTIONS & DESCRIPTION
+             */}
+            <div className={`questions__description`}>
+                <h3>DESCRIPTION HERE:</h3>
+            </div>
+
+            {/* // * BUTTON FOR POSTED QUESTIONS & DESCRIPTION
+             */}
+            <div className={`questions__btn`}>
+                <button>
+                {/* // TODO: IMG icon here.
+                 */}
+                Respond button here
+                </button>
+            </div>
+        </section>
     )
-} 
+}
 
 export default Questions;
