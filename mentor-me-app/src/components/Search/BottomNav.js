69652a745ce9a54b412cb60896cbf6a82965a18c
GUL-Bao <BaoPham92@yahoo.com>
2019-09-25 13:17:47 -0400

    Template for Search component

---
+++ mentor-me-app/src/components/Search/BottomNav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Info = () => (
+    <div>
+        <div className={`nav`}>
+            {/* //? NAV DISPLAY FOR LINKS & ICONS
+             */}
+             <ul>
+                 <li></li>
+                 <li></li>
+                 <li></li>
+                 <li></li>
+             </ul>
+        </div>
+    </div>
+)
---
+++ mentor-me-app/src/components/Search/Info.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Info = () => (
+    <div>
+        <div>
+            {/* //? TOP DISPLAY FOR EXTRA INFO, TIME, ETC.
+             */}
+
+            {/* //? TOP DISPLAY FOR SEARCH INPUT
+             */}
+        </div>
+
+        <div>
+             {/* //? STYLES FOR CATEGORY SELECTION (DATA SORTING)
+              */}
+        </div>
+    </div>
+)
