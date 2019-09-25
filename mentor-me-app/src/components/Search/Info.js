ed8381f5442fcb5dea5b681519c28d341ad2a579
GUL-Bao <BaoPham92@yahoo.com>
2019-09-25 16:04:26 -0400

    Base template for Info top styles complete

--- mentor-me-app/src/components/Search/Info.js
+++ mentor-me-app/src/components/Search/Info.js
@@ -4,26 +4,35 @@ import moment from 'moment';
 // * STYLE IMPORTS (style-components)
 import style from '../Search/StyledSearch';
 
+// * SVG IMPORTS
+import { ReactComponent as Signal } from '../Assets/SVG/signal-solid.svg';
+import { ReactComponent as Wifi } from '../Assets/SVG/wifi-solid.svg';
+import { ReactComponent as Battery } from '../Assets/SVG/battery-full-solid.svg';
+
 export const Info = () => (
     <div>
-        <style.info className={``}>
+        <style.info>
             {/* //? TOP DISPLAY FOR EXTRA INFO, TIME, ETC.
              */}
 
-            <div className={``}>
+            <div className={`info__top`}>
                 <span className={`local-time`}>{moment().format('LT')}</span>
 
                 <div className={`icon-container`}>
+                    <Signal className={`icon-signal`} />
+                    <Wifi className={`icon-wifi`} />
+                    <Battery className={`icon-battery`} />
                 </div>
             </div>
 
             {/* //? TOP DISPLAY FOR SEARCH INPUT
              */}
 
-            <input
-                type="text"
-                value=""
-            />
+            <div className={`info__bottom`}>
+                <input
+                    type="text"
+                    value="" />
+            </div>
         </style.info>
 
         <div>
--- mentor-me-app/src/components/Search/StyledSearch.js
+++ mentor-me-app/src/components/Search/StyledSearch.js
@@ -9,20 +9,50 @@ const section = styled.section`
 const info = styled.div`
     ${'' /* // * INFO DIV LINK STYLES HERE 
     */}
-
-
-    ${'' /* // * ICON STYLES HERE
+    
+    ${'' /* // * INFO TOP CONTAINER
      */}
-    & > .signal {
-        
-    }
+    & > .info__top {
+        padding-top: 1rem;
+        margin: 0 1rem;
 
-    & > .wifi {
-        
-    }
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-between;
 
-    & > .battery {
-        
+        & > .local-time {
+            width: 15%;
+            font-size: 1.6rem;
+            font-weight: 600;
+        }
+
+        ${'' /* // * ICON STYLES HERE
+        */}
+        & > .icon-container {
+            width: 12%;
+
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: space-evenly;
+
+            & > .icon-signal {
+                height: 2vh;
+                width: 3vw;
+            }
+
+            & > .icon-wifi {
+                height: 2vh;
+                width: 3vw;
+            }
+
+            & > .icon-battery {
+                height: 2vh;
+                width: 3vw;
+            }
+        }
+
+        ${'' /* // * INFO BOTTOM CONTAINER
+         */}
     }
 `;
 
