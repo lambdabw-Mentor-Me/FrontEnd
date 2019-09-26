dbda12aed7693c745b49c165bcfe990d404c396d
Michael Harms <michael.harms6010@gmail.com>
2019-09-26 14:32:21 -0500

    centered logo, made it redirect to registration

--- mentor-me-app/src/pages/Home.js
+++ mentor-me-app/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import mainLogo from "../mentor-me-logo.png";
 import styled from "styled-components";
+import {Link} from "react-router-dom";
 
 const Home = () => {
 
@@ -15,7 +16,7 @@ const Home = () => {
     return (
         <div className="welcome">
             <h1>Welcome to Mentor Me!</h1>
-            <img src={mainLogo} />
+            <Link to="/ent-register"><img src={mainLogo} /></Link>
         </div>
     )
 } 
