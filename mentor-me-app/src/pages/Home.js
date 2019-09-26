45a37b446e486d9b73c2878600de22848eca830c
Michael Harms <michael.harms6010@gmail.com>
2019-09-26 14:25:04 -0500

    styled splash page with logo and welcome message

--- mentor-me-app/src/pages/Home.js
+++ mentor-me-app/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import mainLogo from "../mentor-me-logo.png";
+import styled from "styled-components";
 
 const Home = () => {
 
+    const Logo = styled.div`
+        
+        width:200px;
+        background-color:white;
+
+    `
+
+
     return (
-        <h1>Hello from home.js</h1>
+        <div className="welcome">
+            <h1>Welcome to Mentor Me!</h1>
+            <img src={mainLogo} />
+        </div>
     )
 } 
 
